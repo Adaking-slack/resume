fa53c1246bea998e7ec3f8eb971233135392f520
Adaking-slack <85881234+Adaking-slack@users.noreply.github.com>
2021-08-19 18:55:43 +0100

    Add files via upload

---
+++ index.html/question2.js
@@ -0,0 +1,13 @@
+
+const myName = () => {
+    const name = "Emelike Adaeze"
+
+    if (name == "Emelike Adaeze") {
+        return name;
+    } else {
+        return "that is not your name";
+        
+    }
+}
+
+console.log(myName())
